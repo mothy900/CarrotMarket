d5231061049551ae92ea6b0e8ac9570d5561d3b1
mothy900 <mothy900@naver.com>
2023-04-05 02:18:24 +0900

    api

---
+++ libs/client/useMutation.tsx
@@ -0,0 +1,36 @@
+import { type } from "os";
+import { useState } from "react";
+
+interface UseMutation {
+  loading: boolean;
+  data?: object;
+  error?: object;
+}
+
+type UseMutaionResult = [(data: any) => void, UseMutation];
+
+export default function useMutation(url: string): UseMutaionResult {
+  const [state, setState] = useState({
+    loading: false,
+    data: undefined,
+    error: undefined,
+  });
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<undefined | any>(undefined);
+  const [error, setError] = useState<undefined | any>(undefined);
+  function mutation(data: any) {
+    setLoading(true);
+    fetch(url, {
+      method: "POST",
+      headers: {
+        contentType: "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json().catch(() => {}))
+      .then(setData)
+      .catch(setError)
+      .finally(() => setLoading(false));
+  }
+  return [mutation, { loading, data, error }];
+}
